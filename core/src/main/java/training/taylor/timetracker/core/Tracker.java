91e708a8fad4de055851e0183b8707386d9f5007
satishgsk87 <57361693+satishgsk87@users.noreply.github.com>
2019-11-20 22:14:40 +0530

    Update

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -17,8 +17,7 @@ public class Tracker {
 
     public void remove(TimeEntry entry) {
         if (true) {
-            entries.remove(entry);
-            
+            entries.remove(entry);  
         }
 
         entries.remove(entry);
